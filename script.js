c9fbb05bd5404368c91bde03abfb875cc923d4ed
leloufadel <leloufadel@gmail.com>
2024-01-02 13:07:26 +0000

    Add function To Remove todo

--- script.js
+++ script.js
@@ -14,16 +14,51 @@ function addTodo() {
     input.value = "";
     displayTodos()
 }
-function displayTodos() {
-    // got the Item 
-    var listContainer = document.getElementById("todo-list");
 
-    items.forEach(function(todo) {
+
+function createButton(className, iconClass) {
+    var button = document.createElement("button");
+    button.className = className;
+
+    if (iconClass) {
+        var icon = document.createElement("i");
+        icon.classList.add("fa", iconClass);
+        button.appendChild(icon);
+    }
+
+    return button;
+}
+
+function displayTodos() {
+    var listContainer = document.getElementById("todo-list");
+    listContainer.innerHTML = ""; // Clear the existing list
+
+    items.forEach(function(todo, index) {
         var listItem = document.createElement("li");
         listItem.textContent = todo.value;
+
+        var editButton = createButton("editButton", "fa-pencil-square-o");
+        var removeButton = createButton("del-Button", "fa-trash");
+
+        removeButton.onclick = function() {
+            // Call the removeTodo function with the index of the current item
+            removeTodo(index);
+
+        };
+        editButton.onclick = function(){
+            editTodo(index);
+        }
         listContainer.appendChild(listItem);
 
+        listItem.appendChild(editButton);
+        listItem.appendChild(removeButton);
+
     });
+}
+function removeTodo(index){
+    items.splice(index, 1);
+    localStorage.setItem('todo-list', JSON.stringify(items));
+    displayTodos();
 
 }
-displayTodos(); // Call the function to display the updated list
+displayTodos();
